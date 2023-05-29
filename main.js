3b95a34c5d258aab7ed5d07b531080ac8a814894
M3DxM3D <127242136+M3DxM3D@users.noreply.github.com>
2023-05-29 19:57:08 +0200

    challeng_dotenv

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 
 
-const wilder= I am {process.env.NAME}, wilder in {process.env.CITY}, and i love {process.env.LANGUAGE} :D 
+const wilder= ` I am ${process.env.NAME}, wilder in ${process.env.CITY}, and i love ${process.env.LANGUAGE} :D `
 
 console.log(wilder);
 
